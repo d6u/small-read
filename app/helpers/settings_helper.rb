663abe8572a23157802a5bab8fab29cabd20aedd
Daiwei Lu <daiweilu123@gmail.com>
2013-06-04 12:38:44 -0500

    interface for adding and editing folders

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -4,4 +4,8 @@ module SettingsHelper
     '<div class="settings-nav-active-indicator"></div>'.html_safe if params[:action] === action
   end
 
+  def disabled_if_reserved(folder)
+    'disabled'.html_safe if folder.reserved === true
+  end
+
 end
