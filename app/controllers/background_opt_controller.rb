f101cef1fd456d3b0358c7704f0fa72ee7b704e1
Daiwei Lu <daiweilu123@gmail.com>
2013-06-07 14:57:38 -0500

    Address issue for slow mark_all_read process
    1. Change comments style in BackgroundOpt controller

--- app/controllers/background_opt_controller.rb
+++ app/controllers/background_opt_controller.rb
@@ -1,14 +1,21 @@
 class BackgroundOptController < ApplicationController
   # Filter
+  # ======
   before_filter :redirect_if_not_logged_in
 
+
+  # Actions
+  # =======
+
   # refresh
+  # -------
   def refresh
     @user.twitters.each {|tw| tw.refresh_account}
     render :nothing => true
   end
 
   # load_folders_and_feeds
+  # ----------------------
   def load_folders_and_feeds
     json = @user.folders.order("position DESC").inject([]) do |inject, folder|
       inject << folder
@@ -18,7 +25,7 @@ class BackgroundOptController < ApplicationController
   end
 
   # feedback
-  # ========
+  # --------
   def feedback
     redirect_to :action => 'index' unless params[:subject] && params[:content] && params[:page_name]
     # Resque.enqueue(SendFeedback, {
@@ -32,30 +39,33 @@ class BackgroundOptController < ApplicationController
   end
 
   # twitter_api_counts
-  # ==================
+  # ------------------
   def twitter_api_counts
     rate_limit_status = @user.twitters[0].rate_limit_status(:resources => 'statuses')
     render :json => {:limits => rate_limit_status['resources']['statuses']['/statuses/home_timeline']['remaining']}
   end
 
   # welmark_all_readcome
-  # ====================
+  # --------------------
   def mark_all_read
     if params[:feed_id]
       feed = Feed.find_by_id params[:feed_id]
-      feed.tweets.each {|t| t.update_attributes(:read => true)}
-      feed.count_unread
+      feed.tweets.update_all(:read => true)
+      feed.update_attributes(:unread_count => 0)
       feed.folder.count_unread
     elsif params[:folder_id]
       folder = Folder.find_by_id params[:folder_id]
-      folder.tweets.each {|t| t.update_attributes(:read => true)}
-      folder.count_unread(:count_feeds_unread => true)
+      folder.feeds.each do |f|
+        f.tweets.update_all(:read => true)
+        f.update_attributes(:unread_count => 0)
+      end
+      folder.count_unread
     end
     head :no_content
   end
 
   # update_folder_positions
-  # =======================
+  # -----------------------
   def update_folder_positions
     if params[:id_str] && params[:position_str]
       id_strs = params[:id_str].split(',')
@@ -85,4 +95,5 @@ class BackgroundOptController < ApplicationController
     end
   end
 
+# End of BackgroundOptController
 end
