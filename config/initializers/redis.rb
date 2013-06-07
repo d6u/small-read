218f2aeb8f406fac6bd7a93817216171ff2f5d39
Daiwei Lu <daiweilu123@gmail.com>
2013-06-07 02:07:57 -0500

    fix spelling error

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   ENV["REDISTOGO_URL"] ||= "redis://redistogo:9c6e519b0151ab038947d4b72d0dbbf8@spinyfin.redistogo.com:9393/"
-elsif Rails.evn.staging?
+elsif Rails.env.staging?
   ENV["REDISTOGO_URL"] ||= "redis://redistogo:9dc57c99cb50d5ee5d1ff8fc50388741@dory.redistogo.com:10839/"
 else
   ENV["REDISTOGO_URL"] ||= "redis://localhost:6379/"
