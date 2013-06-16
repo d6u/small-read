7a0e2a2eb052fce325869636ad6a9b3dbfcc1440
Daiwei Lu <daiweilu123@gmail.com>
2013-06-15 22:28:26 -0500

    start over: load feeds and folders async

--- app/assets/javascripts/inside/index.js
+++ app/assets/javascripts/inside/index.js
@@ -2,174 +2,43 @@
 //= require angular.min.js
 //= require angular-sanitize.js
 //  require angular-resource.js
-//= require modules/ng-infinite-scroll.js
+//  require modules/ng-infinite-scroll.js
 
 
 // App Init
 // ========
-var app = angular.module('SmallRead', ['ngSanitize', 'infinite-scroll']);
+var app = angular.module('SmallRead', ['ngSanitize']);
 
 // MainCtrl
+// --------
 app.controller(
-    'MainCtrl',
-    function($scope, $http, $attrs) {
-        $scope.showTweets = function(source_type, source_id) {
-            $scope.baseurl = '/' + source_type + '/';
-            $scope.baseurl += source_id + '/tweets/';
-            $http.get($scope.baseurl).success(function(data) {
-                $scope.tweets = angular.forEach(data, function(value, key) {
-                    value.entities = angular.fromJson(value.entities);
-                });
-                $scope.max_tweet_id = data[data.length - 1].id - 1;
-                $scope.reaches_end = data.length < 20 ? true : false;
-            });
-        };
-
-        $scope.busy = false;
-        $scope.scrollToBottom = function() {
-            if ($scope.reaches_end || $scope.busy || typeof $scope.tweets === 'undefined') return;
-            $scope.busy = true;
-            var url = $scope.baseurl + "?max_id=" + $scope.max_tweet_id;
-            $http.get(url).success(function(data) {
-                for (var i = 0; i < data.length; i++) {
-                    data[i].entities = angular.fromJson(data[i].entities);
-                    $scope.tweets.push(data[i]);
-                }
-                if (data.length > 0) $scope.max_tweet_id = data[data.length - 1].id - 1;
-                if (data.length < 20) $scope.reaches_end = true;
-                $scope.busy = false;
-            });
-        };
-
-        // functions
-        $scope.syncTwitter = function() {
-            $scope.refresh_spin = "icon-spin";
-            $http.get('/bg/refresh').success(function(data, status){
-                if (status == 200) console.log("OK");
-                $scope.refresh_spin = null;
-            });
-        };
-    }
-);
-
-// Feeds
-app.directive(
-    "folder",
-    function() {
-        return {
-            controller: function($scope) {
-                $scope.show = 'no-show';
-                $scope.showFeeds = function() {
-                    $scope.show = $scope.show === 'no-show'?'show':'no-show';
-                };
-            },
-            scope: {
-                showTweets: '&'
-            }
-        };
-    }
-);
-
-// Tweets
-app.controller(
-    'TweetsCtrl',
-    function($scope, $element) {
-        $element.children('.tweets-list').on('scroll', function() {
-            $scope.$broadcast(
-                'listScrolling',
-                $element.children('.tweets-list').scrollTop()
-            );
+    'AppCtrl',
+    function($scope, $http) {
+        $http.get('/bg/load_folders_and_feeds.json')
+        .success(function(data) {
+            $scope.folders = data;
         });
     }
 );
 
+// Folder
 app.directive(
-    "tweet",
-    function($compile) {
-        var non_retweet_template = document.querySelector('#tweet-template').innerHTML;
-        var retweet_template = document.querySelector('#retweet-template').innerHTML;
-
-        var getTemplate = function(retweeted_status_id_str) {
-            if (retweeted_status_id_str) {
-                return retweet_template;
-            } else {
-                return non_retweet_template;
-            }
-        };
-
-        var link = function(scope, element, attrs) {
-            element.html(getTemplate(scope.tweet.retweeted_status_id_str));
-            $compile(element.contents())(scope);
-            scope.$on('listScrolling', function(event, parentScrollTop){
-                if (element.position().top < -20) {
-                    scope.markRead();
-                }
-            });
-        };
-
-        var controller = function($scope, $element, $http, $attrs) {
-            $scope.marking_read = false;
-            $scope.markRead = function() {
-                if (!$scope.tweet.read && !$scope.marking_read) {
-                    $scope.marking_read = true;
-                    $scope.tweet.read = true;
-                    $http.put('/tweets/'+$scope.tweet.id, {read: true})
-                    .success(function() {
-                        $element.addClass('read');
-                        $scope.marking_read = false;
-                    });
-                }
-            };
-
-        };
-
+    'folder',
+    function() {
         return {
-            controller: controller,
-            link: link
-            // scope: {}
+            link: function(scope, element, attrs) {
+            }
         };
     }
 );
 
-app.filter(
-    'tweetTextFilter',
+// Feed
+app.directive(
+    'feed',
     function() {
-        return function(input, entities) {
-            // extract and sort entities
-            var entity_array = [];
-            for(var entity in entities) {
-                angular.forEach(entities[entity], function(value, key){
-                    value.type = entity;
-                    entity_array.push(value);
-                });
+        return {
+            link: function(scope, element, attrs) {
             }
-            entity_array.sort(function(a,b){
-                if (a.indices && b.indices) return a.indices[0] - b.indices[0];
-            });
-            // extract input pieces
-            var input_pieces = [];
-            var beginning_point = 0;
-            angular.forEach(entity_array, function(value, key) {
-                input_pieces.push(input.slice(beginning_point, value.indices[0]));
-                switch (value.type) {
-                    case "urls":
-                        input_pieces.push('<a href="'+value.expanded_url+'" target="_blank">'+value.display_url+'</a>');
-                        break;
-                    case "hashtags":
-                        input_pieces.push('<a href="https://twitter.com/search?q=%23'+value.text+'&src=hash" target="_blank>#'+value.text+'</a>');
-                        break;
-                    case "user_mentions":
-                        input_pieces.push('<a href="http://twitter.com/'+value.screen_name+'" target="_blank>@'+value.screen_name+'</a>');
-                        break;
-                    case "medias":
-                        input_pieces.push('<a href="'+value.media_url+'" target="_blank>'+value.display_url+'</a>');
-                        break;
-                }
-                beginning_point = value.indices[1];
-            });
-            input_pieces.push(input.slice(beginning_point));
-            // return input
-            return input_pieces.join("");
         };
     }
 );
