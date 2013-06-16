569a0783322ed3010a42475180e4ce7a16d2a42e
Daiwei Lu <daiweilu123@gmail.com>
2013-06-15 23:24:38 -0500

    load Tweets and parse text

--- app/assets/javascripts/inside/index.js
+++ app/assets/javascripts/inside/index.js
@@ -21,7 +21,7 @@ app.controller(
             });
         };
         $scope.loadFoldersAndFeeds();
-
+        // folder & feed
         $scope.showFeedsList = function(showList) {
             showList.list = showList.list === "show" ? "" : "show";
         };
@@ -29,6 +29,17 @@ app.controller(
             $http.get('/bg/refresh')
             .success(function(data) {
                 $scope.loadFoldersAndFeeds();
+            });
+        };
+        // tweets
+        $scope.tweets = [];
+        $scope.loadTweets = function(type, id) {
+            $http.get('/'+type+'s/'+id+'/tweets')
+            .success(function(data){
+                for (var i = 0; i < data.length; i++) {
+                    data[i].entities = angular.fromJson(data[i].entities);
+                }
+                $scope.tweets = data;
             });
         };
     }
@@ -57,6 +68,77 @@ app.directive(
         return {
             link: function(scope, element, attrs) {
             }
+        };
+    }
+);
+
+//
+app.directive(
+    'tweet',
+    function($compile) {
+        var non_retweet_template = document.querySelector('#tweet-template').innerHTML;
+        var retweet_template = document.querySelector('#retweet-template').innerHTML;
+
+        var getTemplate = function(retweeted_status_id_str) {
+            // TODO: simplify
+            if (retweeted_status_id_str) {
+                return retweet_template;
+            } else {
+                return non_retweet_template;
+            }
+        };
+
+        var link = function(scope, element, attrs) {
+            element.html(getTemplate(scope.tweet.retweeted_status_id_str));
+            $compile(element.contents())(scope);
+        };
+
+        return {
+            link: link
+        };
+    }
+);
+
+// Filters
+app.filter(
+    'tweetTextFilter',
+    function() {
+        return function(input, entities) {
+            // extract and sort entities
+            var entity_array = [];
+            for(var entity in entities) {
+                angular.forEach(entities[entity], function(value, key){
+                    value.type = entity;
+                    entity_array.push(value);
+                });
+            }
+            entity_array.sort(function(a,b){
+                return a.indices[0] - b.indices[0];
+            });
+            // extract input pieces
+            var input_pieces = [];
+            var beginning_point = 0;
+            angular.forEach(entity_array, function(value, key) {
+                input_pieces.push(input.slice(beginning_point, value.indices[0]));
+                switch (value.type) {
+                    case "urls":
+                        input_pieces.push('<a href="'+value.expanded_url+'" target="_blank">'+value.display_url+'</a>');
+                        break;
+                    case "hashtags":
+                        input_pieces.push('<a href="https://twitter.com/search?q=%23'+value.text+'&src=hash" target="_blank>#'+value.text+'</a>');
+                        break;
+                    case "user_mentions":
+                        input_pieces.push('<a href="http://twitter.com/'+value.screen_name+'" target="_blank>@'+value.screen_name+'</a>');
+                        break;
+                    case "medias":
+                        input_pieces.push('<a href="'+value.media_url+'" target="_blank>'+value.display_url+'</a>');
+                        break;
+                }
+                beginning_point = value.indices[1];
+            });
+            input_pieces.push(input.slice(beginning_point));
+            // return input
+            return input_pieces.join("");
         };
     }
 );
