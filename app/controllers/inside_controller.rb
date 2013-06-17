c61dbcbb88a7580731d5714ad2a3237132d674c2
Daiwei Lu <daiweilu123@gmail.com>
2013-06-17 14:34:33 -0500

    before filter to redirect iPhone visitor to mobile view

--- app/controllers/inside_controller.rb
+++ app/controllers/inside_controller.rb
@@ -1,6 +1,7 @@
 class InsideController < ApplicationController
   # Filter
   before_filter :redirect_if_not_logged_in
+  before_filter :redirect_if_mobile
   before_filter :redirect_to_welcome_if_no_email, except: [:welcome, :logout]
   layout "inside_layout"
 
